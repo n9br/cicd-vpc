9a19453b9cf9211c30289324d56b7d08a6ed7437
n9br <ngbr@gmx.net>
2024-08-02 17:16:32 +0200

    clean vpc

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     }
   }
 
-  # Tofu State Bucket
+  # TF State Bucket
   backend "s3" {
     bucket  = "cicd-otf-state"
     key     = "state/express.tfstate"
@@ -20,20 +20,4 @@ terraform {
   }
 }
 
-resource "aws_instance" "express-api" {
-  ami           = "ami-071de147bf3f27475" # Replace with your chosen AMI
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.cicd-subnet-pub-1a.id
-  # associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.cicd-sg.id]
-  key_name               = "0730"
-  user_data              = file("./user_data.sh")
-  tags = {
-    Name = "ExpressAPI"
-  }
-}
-
-output "express-api-ip" {
-  value = aws_instance.express-api.public_ip
-}
 
